5d501a83d6aba823956c6d563a09ae420badc6e8
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-07-18 00:24:41 +0300

    add report body to show daily inome and weekly income

--- main.js
+++ main.js
@@ -12,6 +12,22 @@ function generateReport(){
 
      var dailyIncome = price * dayTotal
      var weeklyIncome = dailyIncome *7
+     var januaryIncome = dailyIncome *31
+     var februaryIncome = dailyIncome *28
+     var febLeapIncome = dailyIncome *29
+     var marchIncome = dailyIncome *31
+     var aprilIncome = dailyIncome *30
+     var mayIncome = dailyIncome *31
+     var juneIncome = dailyIncome *30
+     var julyIncome = dailyIncome *31
+     var augustIncome = dailyIncome *31
+     var septemberIncome = dailyIncome *30
+     var octoberIncome = dailyIncome *31
+     var novemberIncome = dailyIncome *30
+     var decemberIncome = dailyIncome *31
+
+     var leapYearIncome = januaryIncome + febLeapIncome + marchIncome + aprilIncome + mayIncome + juneIncome + julyIncome + augustIncome + septemberIncome + octoberIncome + novemberIncome + decemberIncome 
+     var nonLeapYearIncome = januaryIncome + februaryIncome + marchIncome + aprilIncome + mayIncome + juneIncome + julyIncome + augustIncome + septemberIncome + octoberIncome + novemberIncome + decemberIncome 
     console.log(sheds);
 
 }
