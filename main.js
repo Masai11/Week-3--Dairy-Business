e9fc5ed1ecb3207b4b287fa8c42bed392f725ed3
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-07-18 00:04:30 +0300

    add functionality to calculate the daily income

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function generateReport(){
-    alert("hellow")
+    var sheds = [];
+    sheds[0] = parseInt(document.getElementById('a').value)
+    sheds[1] = parseInt(document.getElementById('b').value)
+    sheds[2] = parseInt(document.getElementById('c').value)
+    sheds[3] = parseInt(document.getElementById('d').value)
+
+    var dayTotal = sheds[0] + sheds[1] + sheds[2] + sheds[3]
+
+     
+     var price  = parseInt(document.getElementById('d').value)
+
+     var dailyIncome = price * dayTotal
+    console.log(sheds);
+
 }
